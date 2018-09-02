4c2b1976c37a64a9859ed5ae764199f30b3132a7
Bruno <bruno97br@gmail.com>
2018-09-02 15:17:20 -0300

    otimizacao e comentarios no codigo

--- code/trabalho.py
+++ code/trabalho.py
@@ -13,9 +13,10 @@ import numpy as np
 import librosa
 import librosa.display
 
+#enderecos de cada pasta de amostras de som
 enderecoDoggos = '../doggos/'
 enderecoSamples = '../samples/'
-centroid = []
+
 medias = []
 desvios = []
 
@@ -23,37 +24,40 @@ print('Flatness')
 
 i=0
 for filename in os.listdir(enderecoDoggos):
+    #descobre a frequencia de amostragem de cada arquivo e carrega no librosa
     framerate = AudioSegment.from_mp3(enderecoDoggos+filename).frame_rate
-    #print(str(i+1) + ": " + str(framerate))
     amostra, fs = (librosa.load(enderecoDoggos+filename, sr=framerate))
 
+    #calcula dft através de dfft, calcula o centroid/flatness e faz a media e o desvio
     transf = (librosa.stft(amostra, n_fft=2048, hop_length=128, win_length=1024, window='hann'))
-    centroid.append(librosa.feature.spectral_flatness(S=np.abs(transf), n_fft=2048,hop_length=512))
-    medias.append(np.mean(centroid[i]))
-    desvios.append(np.std(centroid[i]))
+    centroid = (librosa.feature.spectral_flatness(S=np.abs(transf), n_fft=2048,hop_length=512))
+    medias.append(np.mean(centroid))
+    desvios.append(np.std(centroid))
     i += 1
 
 mediaDoggos  = np.mean ( np.array(medias))
 desvioDoggos = np.mean ( np.array(desvios))
 
+#repete o procedimento para os sons de instrumentos
 medias = []
 desvios = []
-centroid = []
+
 j=i-1;i=0
 for filename in os.listdir(enderecoSamples):
     framerate = AudioSegment.from_mp3(enderecoSamples+filename).frame_rate
-    #print(str(i+1) + ": " + str(framerate))
     amostra, fs = (librosa.load(enderecoSamples+filename, sr=framerate))
 
     transf = (librosa.stft(amostra, n_fft=2048, hop_length=128, win_length=1024, window='hann'))
-    centroid.append(librosa.feature.spectral_flatness(S=np.abs(transf), n_fft=2048,hop_length=512))
-    medias.append(np.mean(centroid[i]))
-    desvios.append(np.std(centroid[i]))
+    centroid = (librosa.feature.spectral_flatness(S=np.abs(transf), n_fft=2048,hop_length=512))
+    medias.append(np.mean(centroid))
+    desvios.append(np.std(centroid))
     i += 1
 
+#calcula a media das medias e a media dos desvios
 mediaSamples  = np.mean ( np.array(medias))
 desvioSamples = np.mean ( np.array(desvios))
 
+#calcula o p-valor
 t, p = st.ttest_ind_from_stats(mediaDoggos, desvioDoggos, j, mediaSamples, desvioSamples, i)
 
 print("Doggos: ",mediaDoggos, desvioDoggos)
